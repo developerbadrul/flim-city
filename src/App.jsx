bf3131eb964a2e3668e70c77e2313133ee66003d
Md Badrul Alam <developer.badrul@gmail.com>
2025-11-03 22:14:21 +0600

    add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
     <>
-      <p className="text-3xl text-center text-red-500">Project initialize </p>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-14">
+          <Sidebar />
+          <div>
+            <h1>Welcome to the Dashboard</h1>
+          </div>
+        </div>
+      </main>
     </>
   );
 };
