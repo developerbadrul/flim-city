fef9fd66f22635f74ed24152a903d25df1532586
Md Badrul Alam <developer.badrul@gmail.com>
2025-11-08 21:00:59 +0600

    create Util for generate Image URL & Succesfully show img in movie card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,12 @@
+import { getImgUrl } from "../../utils/utils";
 import Star from "./../../assets/star.svg"
+import Tag from "./../../assets/tag.svg"
+
 
 const Card = ({ movie }) => {
     return (
         <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-            <img className="w-full object-cover" src="./assets/movie-1.png" alt="" />
+            <img className="w-full object-cover" src={getImgUrl(movie.cover)} alt="" />
             <figcaption className="pt-4">
                 <h3 className="text-xl mb-1">{movie.title}</h3>
                 <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
@@ -16,7 +19,7 @@ const Card = ({ movie }) => {
                 </div>
                 <a className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
                     href="#">
-                    <img src="./assets/tag.svg" alt="" />
+                    <img src={Tag} alt="tag" />
                     <span> ${movie.price} | Add to Cart</span>
                 </a>
             </figcaption>
