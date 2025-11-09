0071ab5c3dfbf6d7e08bf887400c6e520fc89b29
Md Badrul Alam <developer.badrul@gmail.com>
2025-11-09 21:39:19 +0600

    add MOvie details Modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,27 @@ import Logo from './../../assets/logo.svg'
 import Ring from './../../assets/ring.svg'
 import Sun from './../../assets/icons/sun.svg'
 import Moon from './../../assets/icons/moon.svg'
-import Cart from './../../assets/shopping-cart.svg'
+import Cart from './../../assets/icons/checkout.svg'
+import { useState } from 'react'
+import CartDetails from '../Cart/CartDetails'
 
 const Header = () => {
+    const [showCart, setShowCart] = useState(false)
+
+    const handleShowCart = () => {
+        setShowCart(true)
+    }
+
+    const handleCloseCart = () => {
+        setShowCart(false)
+    }
+
     return (
         <header>
+            {showCart &&
+                (<CartDetails
+                    onClose={handleCloseCart}
+                />)}
             <nav className="container flex items-center justify-between space-x-10 py-6">
                 <a href="index.html">
                     <img src={Logo} width="139" height="26" alt="logo" />
@@ -24,7 +40,10 @@ const Header = () => {
                         </a>
                     </li>
                     <li>
-                        <a className="bg-primary/20 dark:bg-primary/7 rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                        <a
+                            className="bg-primary/20 dark:bg-primary/7 rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#"
+                            onClick={handleShowCart}
+                        >
                             <img src={Cart} width="24" height="24" alt="cart" />
                         </a>
                     </li>
