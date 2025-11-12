ab373150631836eaa97ed3ec75b934acd4b8d24f
Md Badrul Alam <developer.badrul@gmail.com>
2025-11-12 12:36:48 +0600

    remove item from cart handle with reducer

--- src/components/Cart/CartDetails.jsx
+++ src/components/Cart/CartDetails.jsx
@@ -7,13 +7,17 @@ import { getImgUrl } from "../../utils/utils";
 
 
 const CartDetails = ({ onClose }) => {
-    const { cartData, setCartData } = useContext(MovieContext)
+    // const { state.cartData, setstate.cartData } = useContext(MovieContext)
+    const { state, dispatch } = useContext(MovieContext)
 
-    const handleDeleteCart = (e, itemId) => {
+
+    const handleDeleteCart = (e, item) => {
         e.preventDefault();
-        const filtered = cartData.filter(item => item.id !== itemId)
 
-        setCartData([...filtered])
+        dispatch({
+            type: "REMOVE_FROM_CART",
+            payload: item
+        })
     }
     return (
         <div
@@ -31,35 +35,35 @@ const CartDetails = ({ onClose }) => {
                     >
 
                         {
-                            cartData.length === 0 ? <p>Cart is empty</p> :
-                            cartData.map(item => (
-                                <div key={item.id} className="grid grid-cols-[1fr_auto] gap-4">
-                                    <div className="flex items-center gap-4">
-                                        <img
-                                            className="rounded overflow-hidden"
-                                            src={getImgUrl(item.cover)}
-                                            alt="Cart"
-                                            width={'50px'}
-                                        />
-                                        <div>
-                                            <h3 className="text-base md:text-xl font-bold">{item.title}</h3>
-                                            <p className="max-md:text-xs text-[#575A6E]">
-                                                {item.genre}
-                                            </p>
-                                            <span className="max-md:text-xs">${item.price}</span>
+                            state.cartData.length === 0 ? <p>Cart is empty</p> :
+                                state.cartData.map(item => (
+                                    <div key={item.id} className="grid grid-cols-[1fr_auto] gap-4">
+                                        <div className="flex items-center gap-4">
+                                            <img
+                                                className="rounded overflow-hidden"
+                                                src={getImgUrl(item.cover)}
+                                                alt="Cart"
+                                                width={'50px'}
+                                            />
+                                            <div>
+                                                <h3 className="text-base md:text-xl font-bold">{item.title}</h3>
+                                                <p className="max-md:text-xs text-[#575A6E]">
+                                                    {item.genre}
+                                                </p>
+                                                <span className="max-md:text-xs">${item.price}</span>
+                                            </div>
+                                        </div>
+                                        <div className="flex justify-between gap-4 items-center">
+                                            <button
+                                                className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white"
+                                                onClick={(e) => handleDeleteCart(e, item)}
+                                            >
+                                                <img className="w-5 h-5" src={Delete} alt="Delete" />
+                                                <span className="max-md:hidden">Remove</span>
+                                            </button>
                                         </div>
                                     </div>
-                                    <div className="flex justify-between gap-4 items-center">
-                                        <button
-                                            className="bg-[#D42967] rounded-md p-2 md:px-4 inline-flex items-center space-x-2 text-white"
-                                            onClick={(e) => handleDeleteCart(e, item.id)}
-                                        >
-                                            <img className="w-5 h-5" src={Delete} alt="Delete" />
-                                            <span className="max-md:hidden">Remove</span>
-                                        </button>
-                                    </div>
-                                </div>
-                            ))
+                                ))
                         }
 
                     </div>
